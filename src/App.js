fed3aa90c1406c576e4a8863742bba349a83fb91
unknown <duongphuhiep@gmail.com>
2016-01-03 18:49:45 +0100

    foundation and assets

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { Router, Route, Link } from 'react-router';
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <nav>
+      <div className="app-container">
+        <nav className="app-nav">
           <ul>
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/casestudies">Case Studiess</Link></li>
           </ul>
         </nav>
-        <div className="content">
+        <div className="app-header"><button>Toggle</button></div>
+        <div className="app-content">
           {this.props.children}
         </div>
       </div>
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,79 @@ import React, { Component } from 'react';
 export default class HomePage extends Component {
   render() {
     return (
-      <div>Home Page</div>
+      <div>
+        <div className="callout large primary">
+          <div className="row column text-center">
+            <h1>Our Blog</h1>
+          </div>
+        </div>
+        <div className="row" id="content">
+          <div className="medium-8 columns">
+            <div className="blog-post">
+              <h3>Awesome blog post title <small>3/6/2015</small></h3>
+              <img ClassName="thumBnail" src="http://placehold.it/850x350" />
+              <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+              <div className="callout">
+                <ul className="menu simple">
+                  <li><a href="#">Author: Mike Mikers</a></li>
+                  <li><a href="#">Comments: 3</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="blog-post">
+              <h3>Awesome blog post title <small>3/6/2015</small></h3>
+              <img ClassName="thumBnail" src="http://placehold.it/850x350" />
+              <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+              <div className="callout">
+                <ul className="menu simple">
+                  <li><a href="#">Author: Mike Mikers</a></li>
+                  <li><a href="#">Comments: 3</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="blog-post">
+              <h3>Awesome blog post title <small>3/6/2015</small></h3>
+              <img ClassName="thumBnail" src="http://placehold.it/850x350" />
+              <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+              <div className="callout">
+                <ul className="menu simple">
+                  <li><a href="#">Author: Mike Mikers</a></li>
+                  <li><a href="#">Comments: 3</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="blog-post">
+              <h3>Awesome blog post title <small>3/6/2015</small></h3>
+              <img ClassName="thumBnail" src="http://placehold.it/850x350" />
+              <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+              <div className="callout">
+                <ul className="menu simple">
+                  <li><a href="#">Author: Mike Mikers</a></li>
+                  <li><a href="#">Comments: 3</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="medium-3 columns" data-sticky-container>
+            <div className="sticky" data-sticky data-anchor="content">
+              <h4>Categories</h4>
+              <ul>
+                <li><a href="#">Skyler</a></li>
+                <li><a href="#">Jesse</a></li>
+                <li><a href="#">Mike</a></li>
+                <li><a href="#">Holly</a></li>
+              </ul>
+              <h4>Authors</h4>
+              <ul>
+                <li><a href="#">Skyler</a></li>
+                <li><a href="#">Jesse</a></li>
+                <li><a href="#">Mike</a></li>
+                <li><a href="#">Holly</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
