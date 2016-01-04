4621a9764ded72f16d3436a7d09d7a958a8d4492
duongphuhiep <duongphuhiep@gmail.com>
2016-01-05 00:53:30 +0100

    try to make a navbar (initial try)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import { Router, Route, Link } from 'react-router';
+import classNames from 'classnames';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {drawerOpen: false};
+  };
+  toogleNavBar() {
+    this.setState({drawerOpen: !this.state.drawerOpen});
+  };
   render() {
     return (
-      <div className="container">
-        <nav>
+      <div className={classNames("app-container", {"drawer-open": this.state.drawerOpen})}>
+        <nav className="app-nav">
           <ul>
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/casestudies">Case Studiess</Link></li>
           </ul>
         </nav>
-        <div className="content">
-          {this.props.children}
+        <div className="inner-wrapper">
+          <header className="app-header">
+            <button onClick={this.toogleNavBar.bind(this)}>Toogle</button>
+          </header>
+          <div className="app-body">
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
--- src/HomePage.js
+++ src/HomePage.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 
 export default class HomePage extends Component {
   render() {
+    let profileBlocks=[];
+
+    //let styl = {display:'block', float:'left', height:"200px"};
+
+    for (let i = 0; i<20; i++) {
+      profileBlocks.push(
+        <div key={i}>
+          <img src='assets/avatar.jpg' />
+          <ul>
+            <li>Name: Profile {i}</li>
+            <li>Age: {i}</li>
+            <li>Status: Normal</li>
+          </ul>
+        </div>
+      )
+    }
+
     return (
-      <div>Home Page</div>
+      <div>
+        <h1>Home page</h1>
+        {profileBlocks}
+      </div>
     );
   }
 }
